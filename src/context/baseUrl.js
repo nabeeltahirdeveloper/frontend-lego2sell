72543ceb82e9cbfa5821e97b3664fc0151cb6ded
Nabeel Tahir <93868996+nabeeltahirdeveloper@users.noreply.github.com>
2023-12-16 01:54:40 +0500

    base update

--- src/context/baseUrl.js
+++ src/context/baseUrl.js
@@ -1,3 +1,3 @@
 // const baseUrl = 'https://backend-api-lego2sell3.vercel.app';
-const baseUrl = 'http://localhost:5100';
+const baseUrl = 'https://backend-api-steel.vercel.app/admin';
 export default baseUrl;
