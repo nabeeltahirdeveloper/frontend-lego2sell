5a9d4d4e28627686ea0a3d45e6dfd4c9c2dafce6
Nabeel <nabeeltahirdeveloper@gmail.com>
2023-12-16 03:48:33 +0500

    Update base URL in baseUrl.js

--- src/context/baseUrl.js
+++ src/context/baseUrl.js
@@ -1,3 +1,3 @@
 // const baseUrl = 'https://backend-api-lego2sell3.vercel.app';
-const baseUrl = 'https://backend-api-steel.vercel.app/admin';
+const baseUrl = 'https://backend-api-steel.vercel.app';
 export default baseUrl;
