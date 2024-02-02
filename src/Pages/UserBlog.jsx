147af68b008205114480ef6ce02228b9dc78c641
Nabeel <nabeeltahirdeveloper@gmail.com>
2024-02-03 02:17:02 +0500

    Update TinyMCE editor configuration

--- src/Pages/UserBlog.jsx
+++ src/Pages/UserBlog.jsx
@@ -328,8 +328,8 @@ const UserBlog = () => {
                     name="description"
       apiKey='j21ua41lr6mtfwtkoqya22hincmd464fz9uviv2k6z633eds'
       init={{
-        plugins: ' tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
-        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight ',
         tinycomments_mode: 'embedded',
         tinycomments_author: 'Author name',
         mergetags_list: [
